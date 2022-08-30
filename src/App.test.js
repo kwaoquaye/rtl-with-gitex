8fa3c675aa8b0a727217c7bdf66c19a1aa65e452
hquaye <humphrey.quaye@persol.net>
2022-08-30 09:06:23 +0000

    feat: :bricks: Added counter feature
    This feature included three buttons namely: Reset, Add, Subtract as well as a display field to show the current value of counter onclick
    
    none

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen,fireEvent } from '@testing-library/react';
+import { Counter } from './Counter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders counter and increments, decrement and resets', () => {
+  const {getByTestId}= render(<Counter />);
+  const linkElement = screen.getByText(/counter/i);
   expect(linkElement).toBeInTheDocument();
+
+  const incrementBtn = getByTestId("increment")
+  const decrementBtn = getByTestId("decrement")
+
+  fireEvent.click(incrementBtn)
+  fireEvent.click(incrementBtn)
+  fireEvent.click(incrementBtn)
+  expect(getByTestId("countVal")).toHaveTextContent("3")
+  fireEvent.click(decrementBtn)
+  expect(getByTestId("countVal")).toHaveTextContent("2")
+
+
 });
