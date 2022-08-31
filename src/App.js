ec36288a04b42f98a23f2bd1d0bbf4f5c35de6fe
hquaye <humphrey.quaye@persol.net>
2022-08-31 15:34:15 +0000

    fleshing out

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import { Counter } from './Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <small>first major release.. We went real public at this point</small>
-      <smal>another bug fix 😪</smal>
+      <header>
+        <h3 className="">Counter App to grasp the gitex workflow extension</h3>
+        <div className="">
+          <article>Authored by:&nbsp;
+            <a href="https://www.linkedin.com/in/humphrey-quaye-8622a2191/" target={"_blank"} rel="author">Quaye&nbsp;Humphrey&#8209;Kwao</a>
+            <time  dateTime="2022-08-30" title="August 30th, 2022"> on 8/30/22</time>
+          </article>
+        </div>
+      </header>
+
+      <main className="">
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter /><br />
+        <small>first major release.. We went real public at this point</small>
+        <small>another bug fix 😪</small>
+      </main>
+      <footer>v1.2.0</footer>
     </div>
+
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,38 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const initCounter = 0
 
 export const Counter = () => {
     const [counterVal, setcounterVal] = useState(initCounter)
-    const handleOnclick = (type) => {
-        switch (type) {
-            case "Increment":
-                return setcounterVal(prev => ++prev)
-            case "Decrement":
-                return setcounterVal(prev => --prev)
-            case "Reset":
-                return setcounterVal(0)
+    const handleOnclick = useCallback(
+        (type) => {
+            switch (type) {
+                case "Increment":
+                    setcounterVal(prev => ++prev)
+                    break
+                case "Decrement":
+                    setcounterVal(prev => --prev)
+                    break
+                case "Reset":
+                    setcounterVal(0)
+                    break
 
 
-            default:
-                break;
-        }
-    }
+                default:
+                    break;
+            }
+        },
+        [counterVal],
+    )
+
+
     return (
         <>
             <div>Counter is:<span data-testid="countVal">{counterVal}</span></div>
             <input type="button" data-testid="increment" onClick={() => handleOnclick("Increment")} value="+" />
             <input type="button" data-testid="decrement" onClick={() => handleOnclick("Decrement")} value="-" />
             <input type="button" onClick={() => handleOnclick("Reset")} value="Reset" />
+           
         </>
     )
 }
